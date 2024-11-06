ca2923bde99989dc0a02e1f1efe33297c42e3f47
DESKTOP-6B7DJIC\장승빈 <jsb031597@naver.com>
2024-11-06 23:55:18 +0900

    UI다듬기/스크롤 템플릿

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 // import { StatusBar } from 'expo-status-bar';
-import React, { useState, useRef } from 'react';
-import { View, Text, TouchableOpacity, StatusBar, ScrollView, Image } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StatusBar, ScrollView, Image, Dimensions } from 'react-native';
 import { styles } from './styles';
 
+const { width } = Dimensions.get('window');
+
 const App = () => {
+  const [isShadowActive, setIsShadowActive] = useState([true, false]); // 상태로 그림자 활성화 여부를 제어
+  const dynamicStyles = [isShadowActive[0]
+    ? { ...styles.shadow1, shadowOpacity: 1 }  // 그림자 활성화 시 opacity 값 설정
+    : { ...styles.shadow1, shadowOpacity: 0 }, 
+    isShadowActive[1]
+    ? { ...styles.shadow2, shadowOpacity: 1 }  // 그림자 활성화 시 opacity 값 설정
+    : { ...styles.shadow2, shadowOpacity: 0 }];
+
+  const [isEnd, setIsEnd] = useState(false);
+  const pagescrollRef = useRef(null);
   const scrollViewRef = useRef(null);
   const [input, setInput] = useState("");
+
+  const dynamicfont = (input.length < 9) 
+    ? {...styles.result1, fontSize: 72}
+    : (input.length == 9) ? {...styles.result1, fontSize: 66}
+    : {...styles.result1, fontSize: 58};
+
   const [result, setResult] = useState("");
+  
+  useEffect(() => {
+    setTimeout(() => {
+      pagescrollRef.current.scrollTo({ x: width * 0.85, animated: false });
+    }, 100);  // 100ms 지연 후에 스크롤 이동
+  }, []);
 
   const handleInput = (value) => {
     setInput(input + value);
     setTimeout(() => {
-      scrollViewRef.current.scrollToEnd({ animated: false } );
+      scrollViewRef.current.scrollToEnd({ animated: false });
     }, 10); // 약간의 지연을 주어 업데이트 후 스크롤
   };
 
@@ -33,6 +57,14 @@ const App = () => {
     setInput(input.slice(0, -1));
   };
 
+  const images = {
+    back: require('./assets/backspace.png'),
+    list: require('./assets/list.png'),
+    calc: require('./assets/calc.png'),
+    comple: require('./assets/comple.png'),
+    // 다른 이미지도 추가 가능
+  };
+
   const renderButton = (style, value, func = handleInput, oper = false) => {
     return (
       <View style={styles.btnbg}>
@@ -43,9 +75,9 @@ const App = () => {
             onPress={() => func(value)}
             style={style}
           >
-            {value=='back' ? <Image
-              source={require('./assets/backspace.png')} // 로컬 이미지 경로
-              style={styles.image} // 이미지 스타일
+            {value in images ? <Image
+              source={images[value]} // 로컬 이미지 경로
+              style={styles[value]} // 이미지 스타일
             /> : <Text style={oper ? styles.ObuttonText : styles.buttonText}>{value}</Text>}
           </TouchableOpacity>
         </View>
@@ -53,58 +85,127 @@ const App = () => {
     );
   };
 
+  const handleScroll = (event) => {
+    const contentWidth = event.nativeEvent.contentSize.width; // 전체 콘텐츠 너비
+    const contentOffsetX = event.nativeEvent.contentOffset.x; // 스크롤 된 위치
+    const layoutWidth = event.nativeEvent.layoutMeasurement.width; // 스크롤 뷰의 너비
+
+
+    if (contentOffsetX <= 0) {
+      // 스크롤이 끝까지 갔을 때
+      if (!isEnd) {
+        setIsEnd(true);
+        console.log('스크롤 끝까지 도달1!');
+        setIsShadowActive([false, true])
+        // 여기서 원하는 함수 호출
+      }
+    } else if (contentWidth - contentOffsetX <= layoutWidth+5) {
+      // 스크롤이 끝까지 갔을 때
+      if (!isEnd) {
+        setIsEnd(true);
+        console.log('스크롤 끝까지 도달2!');
+        setIsShadowActive([true, false])
+        // 여기서 원하는 함수 호출
+      }
+    } else {
+      console.log("dd")
+      setIsEnd(false);
+      if (layoutWidth < contentWidth)
+        setIsShadowActive([true, true])
+    }
+    if (input.length < 8)
+      setIsShadowActive([true, false])
+  };
+
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#2196F3" />
+    <ScrollView
+      ref={pagescrollRef}
+      scrollEnabled={false}
+      horizontal
+      pagingEnabled
+      contentContainerStyle={styles.mainscroll}>
+      <View style={[styles.page, styles.page1]}>
+        <StatusBar barStyle="light-content" backgroundColor="#2196F3" />
 
-      <Text style={styles.result}>{result}</Text>
-
-      <View style={styles.ScrollView}>
-        <ScrollView 
-          ref={scrollViewRef}
-          centerContent
-          horizontal>
-           <Text style={styles.display}>{input || "0"}</Text>
-        </ScrollView>
+        <TouchableOpacity
+          activeOpacity={0.2}
+          style={styles.listcont}
+          onPress={() => pagescrollRef.current.scrollTo({ x: width * 0.85, animated: true })}
+        >
+          <Image
+            source={images.list}
+            style={styles.list} />
+        </TouchableOpacity>
       </View>
+      <View style={[styles.page, styles.page2]}>
+        <StatusBar barStyle="light-content" backgroundColor="#2196F3" />
 
-      <View style={styles.box}>
-        <View style={styles.row}>
-          {input != 0 ? renderButton(styles.button_top, 'back', handleBackspace) : renderButton(styles.button_top, 'AC', clearInput)}
-          {renderButton(styles.button_top, '+/-')}
-          {renderButton(styles.button_top, '%')}
-          {renderButton(styles.buttonOperator, '÷', handleInput, oper = true)}
+        <TouchableOpacity
+          activeOpacity={0.2}
+          style={styles.listcont}
+          onPress={() => pagescrollRef.current.scrollTo({ x: 0, animated: true })}
+        >
+          <Image
+            source={images.list}
+            style={styles.list} />
+        </TouchableOpacity>
+
+        <Text style={styles.result2}>{result}</Text>
+        
+        <View style={styles.display}>
+          <View style={dynamicStyles[0]}></View>
+          <View style={styles.ScrollView}>
+            <ScrollView
+              onScroll={handleScroll} // 스크롤 이벤트 처리
+              scrollEventThrottle={50}
+              scrollEnabled={input.length > 10}
+              ref={scrollViewRef}
+              centerContent
+              horizontal>
+              <Text style={dynamicfont}>{input || "0"}</Text>
+            </ScrollView>
+          </View>
+          <View style={dynamicStyles[1]}></View>
         </View>
 
-        <View style={styles.row}>
-          {renderButton(styles.button, '7')}
-          {renderButton(styles.button, '8')}
-          {renderButton(styles.button, '9')}
-          {renderButton(styles.buttonOperator, '×', handleInput, oper = true)}
-        </View>
+        <View style={styles.box}>
+          <View style={styles.row}>
+            {(!input || result) ?  renderButton(styles.button_top, 'AC', clearInput) : renderButton(styles.button_top, 'back', handleBackspace, false)}
+            {renderButton(styles.button_top, 'comple')}
+            {renderButton(styles.button_top, '%')}
+            {renderButton(styles.buttonOperator, '÷', handleInput, true)}
+          </View>
 
-        <View style={styles.row}>
-          {renderButton(styles.button, '4')}
-          {renderButton(styles.button, '5')}
-          {renderButton(styles.button, '6')}
-          {renderButton(styles.buttonOperator, '−', handleInput, oper = true)}
-        </View>
+          <View style={styles.row}>
+            {renderButton(styles.button, '7')}
+            {renderButton(styles.button, '8')}
+            {renderButton(styles.button, '9')}
+            {renderButton(styles.buttonOperator, '×', handleInput, true)}
+          </View>
 
-        <View style={styles.row}>
-          {renderButton(styles.button, '1')}
-          {renderButton(styles.button, '2')}
-          {renderButton(styles.button, '3')}
-          {renderButton(styles.buttonOperator, '+', handleInput, oper = true)}
-        </View>
+          <View style={styles.row}>
+            {renderButton(styles.button, '4')}
+            {renderButton(styles.button, '5')}
+            {renderButton(styles.button, '6')}
+            {renderButton(styles.buttonOperator, '−', handleInput, true)}
+          </View>
 
-        <View style={styles.row}>
-          {renderButton(styles.button, 'icon', console.log)}
-          {renderButton(styles.button, '0')}
-          {renderButton(styles.button, '.')}
-          {renderButton(styles.buttonOperator, '=', calculateResult, oper = true)}
+          <View style={styles.row}>
+            {renderButton(styles.button, '1')}
+            {renderButton(styles.button, '2')}
+            {renderButton(styles.button, '3')}
+            {renderButton(styles.buttonOperator, '+', handleInput, true)}
+          </View>
+
+          <View style={styles.row}>
+            {renderButton(styles.button, 'calc', console.log)}
+            {renderButton(styles.button, '0')}
+            {renderButton(styles.button, '.')}
+            {renderButton(styles.buttonOperator, '=', calculateResult, true)}
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- styles.js
+++ styles.js
@@ -1,5 +1,6 @@
-import { StyleSheet } from 'react-native';
-import { ScrollView } from 'react-native-web';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 const baseButtonStyle = {
     flex: 1,
@@ -10,24 +11,58 @@ const baseButtonStyle = {
   };
   
   export const styles = StyleSheet.create({
-    container: {
+    mainscroll:{
+      // flex: 1,
+      backgroundColor: '#1c1c1c',
+    },
+    page: {
       flex: 1,
+      height: height,
       justifyContent: 'flex-end',
-      backgroundColor: '#000',
       paddingBottom: 60,
     },
+    page1:{
+      width: width*0.85,
+      backgroundColor: '#1c1c1c',
+    },
+    page2:{
+      width: width,
+      backgroundColor: '#000',
+      borderRadius: 30,
+    },
+    display:{
+      zIndex: 1,
+      flexDirection: 'row',
+    },
     ScrollView: {
+      // flexDirection: 'column',
       alignItems: 'flex-end',
+      // justifyContent: 'center',
+      width: width-32,
       textAlign: 'right',
-      paddingRight: 20,
-      paddingLeft: 20,
       marginBottom: '1%', 
     },
-    display: {
+    shadow1:{
+      zIndex: 1,
+      height: "100%",
+      borderLeftWidth: 16,
+      shadowOffset: { width:10, height: 0 },
+      shadowOpacity: 0,
+      shadowRadius: 4,
+    },
+    shadow2:{
+      zIndex: 1,
+      height: "100%",
+      borderLeftWidth: 16,
+      shadowOffset: { width:-10, height: 0 },
+      shadowOpacity: 0,
+      shadowRadius: 4,
+    },
+    result1: {
       fontSize: 72,
       color: 'white',
     },
-    result: {
+    result2: {
       fontSize: 24,
       color: '#888',
       textAlign: 'right',
@@ -57,8 +92,8 @@ const baseButtonStyle = {
     btnbox2: {
       position:'absolute',
       backgroundColor: 'white',
-      left: 1,
-      top: 1,
+      left: 0.8,
+      top: 0.7,
       height: '98%',
       width: '98%',
       borderRadius: 100,
@@ -82,13 +117,36 @@ const baseButtonStyle = {
     },
     ObuttonText: {
       color: 'white',
-      bottom: 3,
+      bottom: 4,
       fontSize: 54,
       fontWeight: '300',
     },
-    image: {
-      right: 2,
-      width: 50,
-      height:50,
+    back: {
+      // right: 2,
+      // width: 40,
+      height:'44%',
+      aspectRatio: 1, 
+      resizeMode: 'contain',
+    },
+    calc: {
+      height:'42%',
+      aspectRatio: 1, 
+      resizeMode: 'contain',
+    },
+    comple: {
+      height:'33%',
+      aspectRatio: 1, 
+      resizeMode: 'contain',
+    },
+    listcont:{
+      position: 'absolute',
+      top: '8%',
+      left: 0,
+      marginLeft: 18,
+    },
+    list: {
+      height:22,
+      aspectRatio: 1, 
+      resizeMode: 'contain',
     },
   });
